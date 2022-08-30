668bca72f9be7b136dfe4ba4ca4c52c16bd4f85f
Rain <pyc0422@gmail.com>
2022-08-30 16:35:48 -0500

    Complete get to repos endpoint

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const getReposByUsername = require('../helpers/github').getReposByUsername;
 const save = require('../database/index').save;
 const bp = require('body-parser')
+const MongoClient= require('mongodb').MongoClient;
+
 let app = express();
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
@@ -32,6 +34,23 @@ app.post('/repos', function (req, res) {
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+  MongoClient.connect('mongodb://localhost/fetcher', { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
+    if (err) throw err;
+    const database = db.db('fetcher');
+    const repos = database.collection('repos');
+    const cursor = repos.find({}).sort( {forks_count: -1}).limit(25);
+    var reposArr = [];
+    return cursor.forEach(repo => {
+      reposArr.push(repo);
+    })
+    .then(() => {
+      console.log(reposArr.length);
+      res.status(200).send(JSON.stringify(reposArr));
+    })
+
+
+  });
+
 });
 
 let port = 1128;
