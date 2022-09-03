195d508bd0679f51ef6da94c298f6092ef2136ce
Rain <pyc0422@gmail.com>
2022-09-03 14:33:15 -0500

    try to connect mongDb to heroku

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/fetcher', { useNewUrlParser: true, useUnifiedTopology: true });
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
